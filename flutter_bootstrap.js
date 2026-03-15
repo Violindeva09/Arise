3c5f2f6a0cfb0b917c7e51d9f463afca8ff669da
Violindeva09 <Violindeva09@users.noreply.github.com>
2026-03-15 12:41:58 +0000

    deploy: 42f97e9170dee54250d1acb2bf27202e49f43421

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1894314079"
+    serviceWorkerVersion: "2637848566"
   }
 });
